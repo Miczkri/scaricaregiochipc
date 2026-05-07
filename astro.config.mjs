23be7cfbd2b339770fe9cee474f737fa4aea40b1
Miczkri <miczkri@gmail.com>
2026-05-07 19:58:59 +0200

    configtst

--- astro.config.mjs
+++ astro.config.mjs
@@ -45,7 +45,7 @@ const fontsConfig = Object.entries(theme.fonts.font_family)
 export default defineConfig({
   site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
   base: config.site.base_path ? config.site.base_path : "/",
-  trailingSlash: config.site.trailing_slash ? "always" : "never",
+  trailingSlash: "never",
   
   // 1. Dodajemy sekcję build dla Inlining CSS
   build: {
